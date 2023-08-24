e24f6b40fd4a9984690349f9e35b1b48d8bd4a96
VDeshmukhCemtrex <vdeshmukh@cxr.agency>
2023-08-24 19:17:59 +0530

    SCREEN 2 - CODE REFACTOR

--- app/src/main/java/com/example/a7minworkout/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7minworkout/ExerciseActivity.kt
@@ -1,6 +1,7 @@
 package com.example.a7minworkout
 
 import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
@@ -30,32 +31,52 @@ class ExerciseActivity : AppCompatActivity() {
         binding?.toolBarExercise?.setNavigationOnClickListener{
             onBackPressed()
         }
-//        AppInitializer.getInstance(this).initializeComponent(RiveInitializer::class.java)
-//        Rive.init(this@ExerciseActivity)
 
         setupRestView()
 
     }
     private fun setupRestView(){
+//        binding?.ll1?.visibility=View.VISIBLE
+//        binding?.ivGif?.visibility=View.VISIBLE
+//        binding?.tvTitle?.visibility=View.VISIBLE
+        binding?.tvTitle?.text="GET READY FOR EXERCISE"
+//        binding?.flProgressBar?.visibility= View.VISIBLE
+
+//        val layoutParams = binding?.tvTitle?.layoutParams as ConstraintLayout.LayoutParams
+//        layoutParams.bottomToTop = binding?.flProgressBar?.id!!
+//        binding?.tvTitle?.layoutParams=layoutParams
+//        binding?.tvTitle?.requestLayout()
+//        binding?.ll2?.visibility=View.GONE
+//        binding?.flExerciseBar?.visibility= View.GONE
+
         if(restTimer!=null){
             restTimer?.cancel()
             restProgress=0
         }
+
         setRestProgressbar()
     }
     private fun setupExerciseView(){
-//        binding?.ivGif?.visibility=View.GONE
-        binding?.flProgressBar?.visibility= View.GONE
-        binding?.tvTitle?.text=exerciseList!![currentExercisePos].getName()
+//        binding?.ll1?.visibility=View.GONE
+//        binding?.ivGif?.visibility=View.VISIBLE
+//        binding?.tvTitle?.visibility=View.VISIBLE
+//        binding?.flProgressBar?.visibility= View.VISIBLE
+        binding?.progressBar?.max=30
+        binding?.tvTimer?.text=30.toString()
 
-        val layoutParams = binding?.tvTitle?.layoutParams as ConstraintLayout.LayoutParams
-        layoutParams.bottomToTop = binding?.flExerciseBar?.id!!
-        binding?.tvTitle?.layoutParams=layoutParams
-        binding?.tvTitle?.requestLayout()
+        binding?.tvUpExercise?.visibility=View.GONE
+        binding?.tvTitle?.text=exerciseList!![currentExercisePos].getName()
+//        binding?.flExerciseBar?.visibility= View.VISIBLE
+
+//        binding?.ll2?.visibility=View.VISIBLE
+//        binding?.ivGifExercise?.visibility=View.VISIBLE
         binding?.ivGif?.setImageResource(exerciseList!![currentExercisePos].getImage())
 
+//        val layoutParams = binding?.tvTitle?.layoutParams as ConstraintLayout.LayoutParams
+//        layoutParams.bottomToTop = binding?.flExerciseBar?.id!!
+//        binding?.tvTitle?.layoutParams=layoutParams
+//        binding?.tvTitle?.requestLayout()
 
-        binding?.flExerciseBar?.visibility= View.VISIBLE
         if(exerciseTimer!=null){
             exerciseTimer?.cancel()
             exerciseProgress=0
@@ -64,8 +85,9 @@ class ExerciseActivity : AppCompatActivity() {
     }
 
     private fun setRestProgressbar(){
+
         binding?.progressBar?.progress=restProgress
-        restTimer=object:CountDownTimer(10000,1000){
+        restTimer=object:CountDownTimer(3000,1000){
             override fun onTick(p0: Long) {
                 restProgress++
                 binding?.progressBar?.progress=10-restProgress
@@ -80,18 +102,27 @@ class ExerciseActivity : AppCompatActivity() {
         }.start()
     }
     private fun setExerciseProgressbar(){
-        binding?.exerciseBar?.progress=exerciseProgress
-        exerciseTimer=object:CountDownTimer(30000,1000){
+        binding?.progressBar?.progress=exerciseProgress
+        exerciseTimer=object:CountDownTimer(5000,1000){
             override fun onTick(p0: Long) {
                 exerciseProgress++
-                binding?.exerciseBar?.progress=30-exerciseProgress
-                binding?.tvExerciseTimer?.text=(30-exerciseProgress).toString()
+                binding?.progressBar?.progress=30-exerciseProgress
+                binding?.tvTimer?.text=(30-exerciseProgress).toString()
             }
 
             override fun onFinish() {
                 if(currentExercisePos < exerciseList?.size!! - 1){
+//                    binding?.ivGif?.visibility=View.VISIBLE
+                    binding?.ivGif?.setImageResource(R.drawable.animation_break)
+                    binding?.tvUpExercise?.visibility=View.VISIBLE
+                    binding?.tvUpExercise?.text=exerciseList!![currentExercisePos+1].getName()
                     setupRestView()
                 }else {
+//                    binding?.ivGif?.visibility=View.VISIBLE
+//                    binding?.ivGif?.setImageResource(R.drawable.water_fill)
+//                    setupRestView()
+                    val intent= Intent(this@ExerciseActivity,MainActivity::class.java)
+                    startActivity(intent)
                     Toast.makeText(
                         this@ExerciseActivity,
                         "30 sec Exercise completed.",
