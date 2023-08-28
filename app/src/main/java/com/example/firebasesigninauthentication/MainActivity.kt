6b06eb50a85e09f69d5417a5808019d0ed571b03
VDeshmukhCemtrex <vdeshmukh@cxr.agency>
2023-08-28 12:40:09 +0530

    Sign In, Sign out Firebase authentication

---
+++ app/src/main/java/com/example/firebasesigninauthentication/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.firebasesigninauthentication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.firebasesigninauthentication.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+    private var mUsername:String?=null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        mUsername=intent.getStringExtra("Message")
+        binding.tvMessage.text=mUsername
+        firebaseAuth = FirebaseAuth.getInstance()
+        binding.button.setOnClickListener{
+            firebaseAuth.signOut()
+            intent= Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/firebasesigninauthentication/SignUpActivity.kt
@@ -0,0 +1,56 @@
+package com.example.firebasesigninauthentication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.firebasesigninauthentication.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding:ActivitySignUpBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.textView.setOnClickListener {
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.button.setOnClickListener {
+            val email = binding.emailEt.text.toString()
+            val pass = binding.passET.text.toString()
+            val confirmPass = binding.confirmPassEt.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
+                if (pass == confirmPass) {
+
+                    firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            firebaseAuth.signOut()
+                            val intent = Intent(this, SignInActivity::class.java)
+//                            Toast.makeText(this, "$email\nSigned up Successfully.", Toast.LENGTH_SHORT).show()
+                            intent.putExtra("Message", "$email\nSigned up Successfully.")
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                } else {
+                    Toast.makeText(this, "Password mismatch!!", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Field Empty!!!", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+    }
+}
